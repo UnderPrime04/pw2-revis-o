5dcd502c852f5b3722db7609c199d3afa3566e34
Gustavo-Henrique <gustavo.gumesi@gmail.com-henriquefgois06@gmail.com>
2022-03-28 10:45:32 -0300

    Desenvolvimento do exercicio 6

---
+++ src/ExercicioSeis.java
@@ -0,0 +1,25 @@
+
+public class ExercicioSeis {
+
+	public static void main(String[] args) {
+		double pet = 11;
+		
+		if (pet >= 10) 
+		{
+			System.out.println("Peso do pet = " + pet);
+			System.out.println("Pet acima do peso!");
+		}
+		
+		else if (pet <= 4) 
+		{
+			System.out.println("Peso do pet = " + pet);
+			System.out.println("Pet abaixo do peso!");
+		}
+		
+		else 
+		{
+			System.out.println("Peso do pet = " + pet);
+			System.out.println("Pet com peso ideal!");
+		}
+	}
+}
