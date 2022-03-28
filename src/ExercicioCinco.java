56f926a411a251531b53f490facfc1074c286d64
Gustavo-Henrique <gustavo.gumesi@gmail.com-henriquefgois06@gmail.com>
2022-03-28 10:35:24 -0300

    Desenvolvimento do exercicio 5

---
+++ src/ExercicioCinco.java
@@ -0,0 +1,18 @@
+
+public class ExercicioCinco {
+	
+	public static void main(String[] args) {
+			double trabDedds = 5; 
+			double trabDepweb = 8;
+			double trabDebd = 7;
+			double trabDepam = 9;
+
+			double media;
+			
+			media = (trabDepam + trabDebd + trabDepweb + trabDedds) / 4;
+			
+			System.out.println("A media do Aluno é = " + media); 
+		
+
+}
+}
