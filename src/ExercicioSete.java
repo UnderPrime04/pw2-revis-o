c8c30be94667c304935a8a4e635e8e0051668b74
Gustavo-Henrique <gustavo.gumesi@gmail.com-henriquefgois06@gmail.com>
2022-03-28 10:59:47 -0300

    Desenvolvimento do exercicio 8.

---
+++ src/ExercicioSete.java
@@ -0,0 +1,39 @@
+
+public class ExercicioSete {
+
+	public static void main(String[] args) {
+		
+		char dia1 = 6;
+		
+		if (dia1 == 1) 
+		{
+			System.out.println("Segunda feira!! Banco aberto");
+		}
+		
+		else if (dia1 == 2) 
+		{
+			System.out.println("Terca feira!! Banco aberto");
+		}
+		
+		else if (dia1 == 3) 
+		{
+			System.out.println("Quarta feira!! Banco aberto");
+		}
+		
+		else if (dia1 == 4) 
+		{
+			System.out.println("Quinta feira!! Banco aberto");
+		}
+		
+		else if (dia1 == 5) 
+		{
+			System.out.println("Sexta feira!! Banco aberto");
+		}
+		
+		else 
+		{
+			System.out.println("Final de Semana!! Banco Fechado '_' ");
+		
+		}
+	}
+}
